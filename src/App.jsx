c5d13d3d7faaee9674fe6a797980779ef0881214
KuyaAC <aciesnet02@gmail.com>
2025-04-24 17:13:49 +0800

    Added search function

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,15 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
 
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
 
     try{
 
-        const endpoint =  `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+        const endpoint =  query 
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
         const response = await fetch(endpoint, API_OPTIONS);
 
         if(!response.ok){
@@ -38,7 +40,7 @@ const App = () => {
         const data = await response.json();
         
         if(data.Response === 'False') {
-          setErrorMessage(data.Error || 'Failed to fetch movies');
+          setErrorMessage(data.Error || 'Failed to fetch movies');                                                                                              
           setMovieList([]);
           return;
         }
@@ -54,8 +56,8 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
   
 
   return (
